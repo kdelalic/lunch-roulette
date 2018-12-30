d9323e952285424a32c0b7bd7d1e0f3a6283b88f
kdelalic <delalik@mcmaster.ca>
2018-12-30 00:55:57 -0500

    save offset for next session, switch to REST api, etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,130 +1,256 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
-import './App.css';
+import React, {Component} from "react";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import "./App.css";
 
-const BASE_SERVER_URL = "http://localhost:3001"
+const BASE_SERVER_URL = "http://localhost:3001";
+const LIMIT = 50;
+const RESTAURANT_RESET = 900;
 
 class App extends Component {
+    constructor(props) {
+        super(props);
 
-  constructor(props) {
-    super(props) 
+        // window.localStorage.clear();
 
-    this.state = {
-      message: "Getting geolocation information...",
-      offset: 0,
-      limit: 50
+        const prevOffset = parseInt(window.localStorage.getItem("prevOffset"));
+        const prevRestaurants = JSON.parse(
+            window.localStorage.getItem("prevRestaurants")
+        );
+
+        console.log("prevOffset", prevOffset);
+        console.log("prevRestaurants", prevRestaurants);
+
+        this.state = {
+            offset:
+                isNaN(prevOffset) || prevOffset > RESTAURANT_RESET
+                    ? 0
+                    : prevOffset,
+            limit: LIMIT,
+            restaurants: [],
+            fetching: false,
+            prevRestaurants: prevRestaurants || []
+        };
+
+        console.log("offset", this.state.offset);
     }
-  }
 
-  componentDidMount() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
+    // Gets geolocation info if enabled
+    getLocation = () => {
+        if (navigator.geolocation) {
+            this.setState(
+                {
+                    message: "Getting geolocation info..."
+                },
+                () => {
+                    navigator.geolocation.getCurrentPosition(
+                        position => {
+                            this.setState(
+                                {
+                                    coords: {
+                                        latitude: position.coords.latitude,
+                                        longitude: position.coords.longitude
+                                    },
+                                    message: "Getting restaurant information..."
+                                },
+                                () => {
+                                    // Loads restaurants into app
+                                    this.fetchRestaurants(true)
+                                        .then(() => {
+                                            // Displays next restaurants after restaurants are done loading
+                                            this.getNextRestaurant();
+                                        })
+                                        .catch(err => {
+                                            console.log(
+                                                "fetchRestaurants " + err
+                                            );
+                                        });
+                                }
+                            );
+                        },
+                        error => {
+                            let message;
 
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          message: "Getting restaurant information..."
-        }, () => {
-          this.fetchRestaurants();
-        })
-        
-      }, error => {
-        let message;
+                            // Geolocation get errors
+                            switch (error.code) {
+                                case error.PERMISSION_DENIED:
+                                    message =
+                                        "User denied the request for Geolocation.";
+                                    break;
+                                case error.POSITION_UNAVAILABLE:
+                                    message =
+                                        "Location information is unavailable.";
+                                    break;
+                                case error.TIMEOUT:
+                                    message =
+                                        "The request to get user location timed out.";
+                                    break;
+                                case error.UNKNOWN_ERROR:
+                                    message = "An unknown error occurred.";
+                                    break;
+                                default:
+                                    break;
+                            }
 
-        switch(error.code) {
-          case error.PERMISSION_DENIED:
-            message = "User denied the request for Geolocation."
-            break;
-          case error.POSITION_UNAVAILABLE:
-            message = "Location information is unavailable."
-            break;
-          case error.TIMEOUT:
-            message = "The request to get user location timed out."
-            break;
-          case error.UNKNOWN_ERROR:
-            message = "An unknown error occurred."
-            break;
-          default:
-            break;
+                            this.setState({
+                                message: message
+                            });
+                        }
+                    );
+                }
+            );
+        } else {
+            this.setState({
+                message: "Geolocation is not supported by this browser."
+            });
         }
+    };
 
-        this.setState({
-          message: message
-        })
-      });
-    } else {
-      this.setState({
-        message: "Geolocation is not supported by this browser."
-      })
+    // Makes API call to backend to fetch restaurants in bulk
+    fetchRestaurants = firstLoad => {
+        return new Promise((resolve, reject) => {
+            axios
+                .get(
+                    BASE_SERVER_URL +
+                        `/api/restaurants?latitude=${
+                            this.state.coords.latitude
+                        }` +
+                        `&longitude=${this.state.coords.longitude}` +
+                        `&offset=${this.state.offset}` +
+                        `&limit=${this.state.limit}`
+                )
+                .then(res => {
+                    this.setState(
+                        prevState => {
+                            window.localStorage.setItem(
+                                "prevOffset",
+                                prevState.offset
+                            );
+
+                            let restaurants;
+                            let prevRestaurants = this.state.prevRestaurants;
+
+                            if (firstLoad) {
+                                restaurants = res.data.filter(value => {
+                                    return !prevRestaurants.includes(value.id);
+                                });
+                            } else {
+                                restaurants = res.data;
+                                prevRestaurants = [];
+                            }
+
+                            return {
+                                restaurants: restaurants.concat(
+                                    prevState.restaurants
+                                ),
+                                message: null,
+                                fetching: false,
+                                offset: prevState.offset + prevState.limit,
+                                prevRestaurants: prevRestaurants
+                            };
+                        },
+                        () => {
+                            resolve();
+                        }
+                    );
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    };
+
+    // Displays next restaurant
+    getNextRestaurant = () => {
+        if (
+            !this.state.message &&
+            this.state.restaurants.length <=
+                Math.round(this.state.limit * 0.2) &&
+            !this.state.fetching
+        ) {
+            this.setState(
+                {
+                    fetching: true
+                },
+                () => {
+                    this.getNextRestaurant();
+                    this.fetchRestaurants();
+                }
+            );
+        } else {
+            let randomNumber = this.getRandomNumber(
+                this.state.restaurants.length
+            );
+            let restaurant = this.state.restaurants[randomNumber];
+
+            let prevRestaurants = this.state.prevRestaurants;
+            prevRestaurants.push(restaurant.id);
+
+            window.localStorage.setItem(
+                "prevRestaurants",
+                JSON.stringify(prevRestaurants)
+            );
+
+            this.setState(
+                prevState => ({
+                    restaurant: {
+                        name: restaurant.name,
+                        rating: restaurant.rating,
+                        location: restaurant.location.address1
+                    },
+                    restaurants: prevState.restaurants.filter(
+                        (_, i) => i !== randomNumber
+                    ),
+                    message: null,
+                    prevRestaurants: prevRestaurants
+                }),
+                () => {
+                    console.log(this.state.restaurants);
+                }
+            );
+        }
+    };
+
+    getRandomNumber = max => {
+        return Math.floor(Math.random() * max);
+    };
+
+    render() {
+        if (!this.state.message && !this.state.coords) {
+            return (
+                <div className="App">
+                    <Button
+                        onClick={this.getLocation}
+                        variant="contained"
+                        color="primary"
+                    >
+                        Show nearby restaurants{" "}
+                    </Button>{" "}
+                </div>
+            );
+        } else if (!this.state.message && this.state.restaurant) {
+            return (
+                <div className="App">
+                    <h2> {this.state.restaurant.name} </h2>{" "}
+                    <h2> {this.state.restaurant.rating} </h2>{" "}
+                    <h2> {this.state.restaurant.location} </h2>{" "}
+                    <Button
+                        onClick={this.getNextRestaurant}
+                        variant="contained"
+                        color="primary"
+                    >
+                        Shuffle{" "}
+                    </Button>{" "}
+                </div>
+            );
+        } else {
+            return (
+                <div className="App">
+                    <h2> {this.state.message} </h2>{" "}
+                </div>
+            );
+        }
     }
-  }
-
-  fetchRestaurants = () => {
-    axios.get(BASE_SERVER_URL + "/api/graphql/restaurants?latitude=" + this.state.latitude + "&longitude=" + this.state.longitude + "&offset=" + this.state.offset + "&limit=" + this.state.limit)
-    .then(res => {
-      this.setState({
-        restaurants: res.data,
-        message: null
-      }, () => {
-        this.getNextRestaurant();
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-
-  getNextRestaurant = () => {
-    if (this.state.restaurants.length === 0) {
-      this.setState(prevState => ({
-        message: "Getting restaurant information...",
-        offset: prevState.offset + prevState.limit
-      }), () => {
-        this.fetchRestaurants();
-      });
-    } else {
-      let randomNumber = this.getRandomNumber(this.state.restaurants.length);
-      let restaurant = this.state.restaurants[randomNumber];
-
-      this.setState(prevState => ({
-        restaurant: {
-          name: restaurant.name,
-          rating: restaurant.rating,
-          location: restaurant.location.address1
-        },
-        restaurants: prevState.restaurants.filter((_, i) => i !== randomNumber),
-        message: null
-      }), () => {
-        console.log(this.state.restaurants)
-      });
-    }
-  }
-
-  getRandomNumber = (max) => {
-    return Math.floor(Math.random() * max);
-  }
-
-  render() {
-    if(!this.state.message && this.state.restaurant) {
-      return (
-        <div className="App">
-          <h2>{this.state.restaurant.name}</h2>
-          <h2>{this.state.restaurant.rating}</h2>
-          <h2>{this.state.restaurant.location}</h2>
-          <Button onClick={this.getNextRestaurant} variant="contained" color="primary">
-            Shuffle
-          </Button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <h2>{this.state.message}</h2>
-        </div>
-      );
-    }
-  }
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 3001;
 
-app.use(cors());
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200
+};
 
-app.get('/api/graphql/restaurants', (req, res) => {
-  
-  const latitude = req.query.latitude;
-  const longitude = req.query.longitude;
-  const offset = req.query.offset;
-  const limit = req.query.limit;
+app.use(cors(corsOptions));
 
-  axios({
-    url: 'https://api.yelp.com/v3/graphql',
-    method: 'post',
-    headers: { 'Authorization': 'Bearer tKcIaEyXmxDUFrtM3EYISnLxsI22snBqDH9x6yTAXnB_ZhAz-DB_k4BzIJGlcm9-EFO94wQVxQRuqFZp0M_I8EoDFDHlYXGeXmHD44Sk6LSM1LfUlfAu8ZInwk78W3Yx',
-               'Content-Type': 'application/graphql' },
-    data: 
-      `{
-        search(term: "restaurants", latitude: ` + latitude + `, longitude: ` + longitude + `, limit: ` + limit + `, offset: ` + offset + `) {
+app.get("/api/graphql/restaurants", (req, res) => {
+    const latitude = req.query.latitude;
+    const longitude = req.query.longitude;
+    const offset = req.query.offset;
+    const limit = req.query.limit;
+
+    axios({
+        url: "https://api.yelp.com/v3/graphql",
+        method: "post",
+        headers: {
+            Authorization:
+                "Bearer tKcIaEyXmxDUFrtM3EYISnLxsI22snBqDH9x6yTAXnB_ZhAz-DB_k4BzIJGlcm9-EFO94wQVxQRuqFZp0M_I8EoDFDHlYXGeXmHD44Sk6LSM1LfUlfAu8ZInwk78W3Yx",
+            "Content-Type": "application/graphql"
+        },
+        data: `{
+        search(term: "restaurants", latitude: ${latitude}, longitude: ${longitude}, limit: ${limit}, offset: ${offset}) {
           business {
             name
             rating
@@ -32,30 +38,46 @@ app.get('/api/graphql/restaurants', (req, res) => {
           }
         }
       }`
-  }).then((result) => {
-    res.send(result.data.data.search.business);
-  }).catch((err) => {
-    console.log("GET/api/graphql/restaurants " + err);
-  });
+    })
+        .then(result => {
+            res.send(result.data.data.search.business);
+        })
+        .catch(err => {
+            console.log("GET/api/graphql/restaurants " + err);
+        });
 });
 
-app.get('/api/restaurants', (req, res) => {
-  
-  const latitude = req.query.latitude;
-  const longitude = req.query.longitude;
-  const offset = req.query.offset;
-  const limit = req.query.limit;
+app.get("/api/restaurants", (req, res) => {
+    const term = "restaurants";
+    const latitude = req.query.latitude;
+    const longitude = req.query.longitude;
+    const offset = req.query.offset;
+    const limit = req.query.limit;
 
-  axios({
-    url: 'https://api.yelp.com/v3/businesses/search?term=burritos&latitude=' + latitude + '&longitude=' + longitude + '&offset=' + offset + '&limit=' + limit,
-    method: 'get',
-    headers: { 'Authorization': 'Bearer tKcIaEyXmxDUFrtM3EYISnLxsI22snBqDH9x6yTAXnB_ZhAz-DB_k4BzIJGlcm9-EFO94wQVxQRuqFZp0M_I8EoDFDHlYXGeXmHD44Sk6LSM1LfUlfAu8ZInwk78W3Yx'}
-  }).then((result) => {
-    res.send(result.data.businesses);
-  }).catch((err) => {
-    console.log("GET/api/restaurants " + err);
-  });
+    const url =
+        `https://api.yelp.com/v3/businesses/search` +
+        `?term=${term}` +
+        `&latitude=${latitude}` +
+        `&longitude=${longitude}` +
+        `&offset=${offset}` +
+        `&limit=${limit}`;
+
+    console.log(url);
+
+    axios({
+        url: url,
+        method: "get",
+        headers: {
+            Authorization:
+                "Bearer tKcIaEyXmxDUFrtM3EYISnLxsI22snBqDH9x6yTAXnB_ZhAz-DB_k4BzIJGlcm9-EFO94wQVxQRuqFZp0M_I8EoDFDHlYXGeXmHD44Sk6LSM1LfUlfAu8ZInwk78W3Yx"
+        }
+    })
+        .then(result => {
+            res.send(result.data.businesses);
+        })
+        .catch(err => {
+            console.log("GET/api/restaurants " + err);
+        });
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
